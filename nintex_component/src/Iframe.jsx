b28ce1314a7734394c2885d72bcfa60a5fd5719a
AUTOMATED\henrylau <henrylau@asl.com.hk>
2023-05-10 17:18:19 +0800

    pie

--- nintex_component/src/Iframe.jsx
+++ nintex_component/src/Iframe.jsx
@@ -1,3 +1,41 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+
+export function PieChart() {
+  return <Pie data={data} />;
+}
+
 export const IFrame = ({
   name = "wikipedia",
   title = "Wikipedia",
@@ -6,7 +44,7 @@ export const IFrame = ({
 }) => {
   const styles = `
   :host {
-    height: 100%;
+    height: 50%;
     width: 100%;
     display: block;
   }
@@ -30,6 +68,7 @@ export const IFrame = ({
         title={title}
         src={src}
       ></iframe>
+      <PieChart/>
     </>
   );
 };
